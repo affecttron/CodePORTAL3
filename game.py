c1065422409fb872990d25125ee9a77be87aa4af
AffectTron <affecttrons@inbox.lv>
2026-05-13 19:27:39 +0300

    imports and data

--- game.py
+++ game.py
@@ -1,26 +1,18 @@
-
+import pygame
+import sys
+import os
 from player import Player
-from level import ConditionLevel, LoopLevel, FunctionLevel
+from player_sprite import PlayerSprite
+from world import World
+from camera import Camera
+from tile_registry import TileRegistry
+from parallax_background import ParallaxBackground
+from level import create_level
 from score_log import ScoreLog
-
-
-class Game:
-    def __init__(self, title="CODE Portal 3"):
-        self._title = title
-        self._current_level = 0
-        self._is_running = False
-        self._levels = []
-        self._player = None
-        self._score_log = ScoreLog()
-
-    def start_game(self):
-        pass
-
-    def load_levels(self):
-        pass
-
-    def next_level(self):
-        pass
-
-    def end_game(self):
-        pass
+from settings import (
+    SCREEN_WIDTH, SCREEN_HEIGHT, FPS, TITLE, BACKGROUND_COLOR,
+    PLAYER_SPAWN_X, PLAYER_SPAWN_Y,
+    WHITE, BLACK, NEON_CYAN, NEON_PINK, NEON_GREEN, NEON_RED, NEON_YELLOW,
+    DARK_GRAY, GRAY,
+    LEVELS_FOLDER,
+)
